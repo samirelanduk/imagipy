469672d62b0ac1392081d40ffed0e4451fe3f6f9
Sam Ireland <sam.ireland.uk@gmail.com>
2017-10-22 16:49:16 +0100

    Add Color impoprt

--- imagipy/__init__.py
+++ imagipy/__init__.py
@@ -1,2 +1,4 @@
+from .models import Color
+
 __author__ = "Sam Ireland"
 __version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setup(name="imagipy",
                    "Programming Language :: Python :: 3.4",
                    "Programming Language :: Python :: 3.5",
                    "Programming Language :: Python :: 3.6"],
-      packages=["imagipy"],
+      packages=["imagipy", "imagipy.models"],
       install_requires=[])
